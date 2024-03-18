a4fbc11b90e01002bc19b69e541092c0d9c5163b
gwyncmn <gwyn2fa@toadware.ca>
2024-03-18 10:08:09 -0400

    login

---
+++ clock.vue
@@ -0,0 +1,27 @@
+<script>
+  methods: {
+    login(user) {
+      this.user = user
+      },
+
+    getVersion() {
+      baseReload('/freeloader/version.json').then(
+        (resp) => { this.version = resp.version; console.log("version ",this.version) }
+      )},
+
+      async function startClock() {
+      //var servtime = await baseReload(BASESCHED + `/server/time`);
+      this.servtime = new Date()
+      //this.servtime = parseInt(servtime.servtime);
+      setInterval(this.updtTimer, 1000);
+    },
+
+    updtTimer() {
+      this.servtime += 1000;
+      var now = new Date(this.servtime).getTime();
+      var ND = new Date(now).toLocaleString("en-CA", { hour12: false });
+      this.timer = ND.slice(0, 20).replace("T", " ");
+    },
+  }
+  </script>
+
